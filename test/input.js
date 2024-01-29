9e532db311d97fd793414153ac64f787035d50b9
Yogeshwari-kaar <156830233+Yogeshwari-kaar@users.noreply.github.com>
2024-01-29 10:57:28 +0530

    Add files via upload

---
+++ test/input.js
@@ -0,0 +1,650 @@
+data = [ { 
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c089c"
+  },
+  "inid": "d3a9fe0c-98c5-4c21-bb82-56b513b0a8f7",
+  "orderID": "1",
+  "itemID": "lockternBP-001",
+  "processItemID": null,
+  "title": "Manufacturing",
+  "description": null,
+  "status": "new",
+  "processLevel": "lineofbusiness", 
+  "refGSIID": "",
+  "assignedTo": "person1",
+  "participants": [
+    {
+      "$oid": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ],
+  "value": 5,
+  "velocity": 5,
+  "volume": 5,
+  "materiality": 5,
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": "yogi",
+  "lastEditedBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "lastEditedOn": {
+    "$date": "2023-11-23T18:37:24.956Z"
+  },
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Manufacturing",
+      "workitem": "GSI",
+      "msg": "Process item - lineofbusiness is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c089c"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.681Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c089d"
+  },
+  "inid": "c0f0de00-d905-4d94-b2df-340ba315d884",
+  "orderID": "1.1",
+  "itemID": "lockternBP-002",
+  "processItemID": "",
+  "title": "Master Data",
+  "description": "",
+  "status":"completed",
+  "processLevel": "processgroup",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c089c"
+  },
+  "assignedTo": "person2",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.681Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Master Data",
+      "workitem": "GSI",
+      "msg": "Process item - processgroup is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c089d"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.681Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c089e"
+  },
+  "inid": "5a7d327b-58ed-425c-aab6-3d4630278117",
+  "orderID": "1.1.1",
+  "itemID": "lockternBP-003",
+  "processItemID": "",
+  "title": "Master data setup and basic setup",
+  "description": "",
+  "status":"approved",
+  "processLevel": "scopeitem",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c089d"
+  },
+  "assignedTo": "person3",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Master data setup and basic setup",
+      "workitem": "GSI",
+      "msg": "Process item - scopeitem is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c089e"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c089f"
+  },
+  "inid": "480be934-9d37-43b7-909c-19131b41def8",
+  "orderID": "1.1.1.1",
+  "itemID": "lockternBP-004",
+  "processItemID": "",
+  "title": "Material Master",
+  "description": "",
+  "status":"active",
+  "processLevel": "scopeitemvariant",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c089e"
+  },
+  "assignedTo": "person2",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Material Master",
+      "workitem": "GSI",
+      "msg": "Process item - scopeitemvariant is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c089f"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    },
+    {
+      "type": "Link",
+      "title": " Conduct Workshop  -  Sourcing & Procurement ",
+      "workitem": "Task",
+      "msg": "Process item is linked to the task -  Conduct Workshop  -  Sourcing & Procurement  by KTern Admin",
+      "_id": {
+        "$oid": "654c7d8db262a0b290feb606"
+      },
+      "status": "Active",
+      "statusCategory": "Active",
+      "date": {
+        "$date": "2023-12-11T22:22:41.034Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a0"
+  },
+  "inid": "408744aa-11ba-4c4e-a1ce-e797760284e6",
+  "orderID": "1.1.1.1.1",
+  "itemID": "lockternBP-005",
+  "processItemID": "",
+  "title": "Create Material Master",
+  "description": "",
+  "status":"completed",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c089f"
+  },
+  "assignedTo": "person3",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Create Material Master",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a0"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a1"
+  },
+  "inid": "e4883882-64cc-489b-b761-390e2dcba28a",
+  "orderID": "1.1.1.1.2",
+  "itemID": "lockternBP-006",
+  "processItemID": "",
+  "title": "Display Material Master",
+  "description": "",
+  "status":"new",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c089f"
+  },
+  "assignedTo": "person3",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Display Material Master",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a1"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a2"
+  },
+  "inid": "b1fee3bb-17e3-4bfd-9d56-faf34a1d087b",
+  "orderID": "1.1.1.2",
+  "itemID": "lockternBP-007",
+  "processItemID": "",
+  "title": "Bill of Materials",
+  "description": "",
+  "status":"new",
+  "processLevel": "scopeitemvariant",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c089e"
+  },
+  "assignedTo": "person2",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Bill of Materials",
+      "workitem": "GSI",
+      "msg": "Process item - scopeitemvariant is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a2"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a3"
+  },
+  "inid": "544618cb-2fb5-4aa9-9d89-c9d9747e4209",
+  "orderID": "1.1.1.2.1",
+  "itemID": "lockternBP-008",
+  "processItemID": "",
+  "title": "Create/Change/Display  BOM",
+  "description": "",
+  "status":"completed",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c08a2"
+  },
+  "assignedTo": "person1",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Create/Change/Display  BOM",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a3"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a4"
+  },
+  "inid": "adec7ccb-5dfc-4ae3-bc9e-6d3a13faff5c",
+  "orderID": "1.1.1.2.2",
+  "itemID": "lockternBP-009",
+  "processItemID": "",
+  "title": "Mass Change  of BOM ",
+  "description": "",
+  "status":"active",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c08a2"
+  },
+  "assignedTo": "person2",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Mass Change  of BOM ",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a4"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a5"
+  },
+  "inid": "f5213f33-949d-4bdc-b272-05b8c9c2fba9",
+  "orderID": "1.1.1.2.3",
+  "itemID": "lockternBP-010",
+  "processItemID": "",
+  "title": "Multilevel BOM",
+  "description": "",
+  "status": "approved",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c08a2"
+  },
+  "assignedTo": "person3",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Multilevel BOM",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a5"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a6"
+  },
+  "inid": "12900ed5-9c2e-4584-bf63-b976c4eefd7c",
+  "orderID": "1.1.1.2.4",
+  "itemID": "lockternBP-011",
+  "processItemID": "",
+  "title": "BOM Level by Level",
+  "description": "",
+  "status": "new",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c08a2"
+  },
+  "assignedTo": "person1",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "BOM Level by Level",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a6"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+},
+
+
+{
+  "_id": {
+    "$oid": "649d0f44d6df68e9fc4c08a7"
+  },
+  "inid": "192222e6-c539-42df-9ec1-21c44e4f96ca",
+  "orderID": "1.1.1.2.5",
+  "itemID": "lockternBP-012",
+  "processItemID": "",
+  "title": "Summarized BOM",
+  "description": "",
+  "status": "completed",
+  "processLevel": "processstep",
+  "refGSIID": {
+    "$oid": "649d0f44d6df68e9fc4c08a2"
+  },
+  "assignedTo": "person3",
+  "participants": [
+    ""
+  ],
+  "value": "",
+  "velocity": "",
+  "volume": "",
+  "materiality": "",
+  "createdBy": {
+    "$oid": "5ccea0fb6d01b0599e0f1d47"
+  },
+  "createdOn": {
+    "$date": "2023-06-29T04:57:40.682Z"
+  },
+  "lastEditedBy": "",
+  "lastEditedOn": "",
+  "active": true,
+  "timeStreams": [
+    {
+      "type": "Add",
+      "title": "Summarized BOM",
+      "workitem": "GSI",
+      "msg": "Process item - processstep is created by KTern Admin",
+      "_id": {
+        "$oid": "649d0f44d6df68e9fc4c08a7"
+      },
+      "status": {
+        "$oid": "5f7d7ebc230b084c9c9b6338"
+      },
+      "statusCategory": "New",
+      "date": {
+        "$date": "2023-06-29T04:57:40.682Z"
+      },
+      "user": "5ccea0fb6d01b0599e0f1d47"
+    }
+  ]
+}
+
+]
+
+module.exports.data = data;
